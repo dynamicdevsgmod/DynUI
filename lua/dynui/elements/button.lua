0ae9e2deda45a4fb9656bd0352260f6411d7c57c
JoshJOkayguy <brsimoneau21@gmail.com>
2021-07-03 07:53:40 -0400

    Depressed buttons look nicer
    Hopefully they won't be as depressed now

--- lua/dynui/elements/button.lua
+++ lua/dynui/elements/button.lua
@@ -9,7 +9,7 @@ function PANEL:Paint(w,h)
     draw.RoundedBox(6, 0, 2, w, h - 4, self.darker_color or color_white)
 
     if self:IsHovered() and input.IsMouseDown(MOUSE_LEFT) then
-        draw.RoundedBox(6, 0, 1, w, h - 2, self.Color or color_white)
+        draw.RoundedBox(6, 0, 1, w, h - 4, self.Color or color_white)
         draw.SimpleText(self.DText or "Button", "DynUI_Button", w * .5, h * .44, self.TextColor or color_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
     else
         draw.RoundedBox(6, 0, 0, w, h - 6, self.Color or color_white)
