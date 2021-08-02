817758eb6f6e25150e05d43f2f18f11605c4f060
JoshJOkayguy <brsimoneau21@gmail.com>
2021-08-02 01:37:35 -0400

    feat: TextEntry nearing completion

--- lua/dynui/elements/textentry.lua
+++ lua/dynui/elements/textentry.lua
@@ -4,34 +4,61 @@ function PANEL:Init()
     self.Font = "DynUI_Title"
     self.NoFocusColor = DynUI:DarkenColor(color_white, 70)
     self.TextColor = color_white
-
-    self.Color = self.TextColor
     self.HighlightColor = Color(18,110,196)
-
+    self.AccentColor = Color(255,255,255)
     self:SetCursor("beam")
+
+    self.Disabled = false
     
-    local text = self:Add("DTextEntry")
-    text:Dock(FILL)
-    text:DockMargin(0,0,0,2)
-    text:SetDrawLanguageID(false)
-    text:SetFont(self.Font)
-    text.Paint = function(me,w,h)
-        me:DrawTextEntryText(self:GetTextColor(), self.HighlightColor, color_white)
+    self.Decoration = self:Add("DPanel")
+    self.Decoration:Dock(BOTTOM)
+    self.Decoration:SetTall(2)
+    self.Decoration:SetBackgroundColor(self.AccentColor)
+    self.Decoration.SetColor = self.Decoration.SetBackgroundColor
+    self.Decoration.GetColor = self.Decoration.GetBackgroundColor
+
+    self.TextEntry = self:Add("DTextEntry")
+    self.TextEntry:Dock(FILL)
+    self.TextEntry:DockMargin(0,2,2,2)
+    self.TextEntry:SetDrawLanguageID(false)
+    self.TextEntry:SetFont(self.Font)
+    self.TextEntry.Paint = function(me,w,h)
+        me:DrawTextEntryText(self.TextColor, self.HighlightColor, color_white)
+    end
+    self.TextEntry.OnFocusChanged = function(_me, focus)
+        if focus then 
+            self.Decoration:ColorTo(DynUI:DarkenColor(self.AccentColor), .25, 0)
+        else
+            self.Decoration:ColorTo(self.AccentColor, .25, 0)
+        end
     end
 end
 
-function PANEL:Paint(w,h)
-    surface.SetDrawColor(Color(177,51,51))
-    surface.DrawRect(0, h - 2, w, 2)
-end
+function PANEL:SetText(text) self.TextEntry:SetText(text) end
+function PANEL:GetText() return self.TextEntry:GetText() end
 
-function PANEL:SetText(text) self.Text = text end
-function PANEL:GetText() return self.Text end
-
-function PANEL:SetFont(font) self.Font = font end
+function PANEL:SetFont(font) self.Font = font self.TextEntry:SetFont(font) end
 function PANEL:GetFont() return self.Font end
 
-function PANEL:SetTextColor(color) self.TextColor = color end
+function PANEL:SetTextColor(color) self.TextColor = color self.TextEntry:SetTextColor(color) end
 function PANEL:GetTextColor() return self.TextColor end
+
+function PANEL:SetAccentColor(color) self.AccentColor = color self.Decoration:SetBackgroundColor(self.AccentColor) end
+function PANEL:GetAccentColor() return self.AccentColor end
+
+function PANEL:SetDisabled(disable)
+    self:SetKeyboardInputEnabled(!disable)
+    self.TextEntry:SetMouseInputEnabled(!disable)
+
+    if disable then
+        self:SetCursor("no")
+        self.TextEntry:SetCursor("no")
+    else
+        self:SetCursor("beam")
+        self.TextEntry:SetCursor("beam")
+    end
+end
+
+function PANEL:GetDisabled() return self.Disabled end
 
 derma.DefineControl("DynTextEntry", "Dynamic VGUI Text Field", PANEL, "Panel")
