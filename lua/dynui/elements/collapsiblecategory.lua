8d5551cb4da0369536dc586701e46ae53c2a0ae2
JoshJOkayguy <brsimoneau21@gmail.com>
2021-08-04 06:57:08 -0400

    feat: ColCat base features and look

--- lua/dynui/elements/collapsiblecategory.lua
+++ lua/dynui/elements/collapsiblecategory.lua
@@ -1,6 +1,52 @@
 local PANEL = {}
 
 function PANEL:Init()
+    self.Color = DynUI.Sidebar.Primary
+
+    if self:GetExpanded() then
+        self.Header.VBarHeight = 0
+    else
+        self.Header.VBarHeight = 5
+    end
+
+    self:SetHeaderHeight(30)
+    self.Header:SetFont("DynUI_Title")
+    self.Header.Paint = function(me,w,h)
+        surface.SetDrawColor(self.Color)
+        surface.DrawRect(0,0,w,h)
+                
+        surface.SetDrawColor(color_white)
+        surface.DrawRect(w - 30, h * .5, 12, 2)
+
+        if self:GetExpanded() then
+            
+        else
+            surface.DrawRect(w - 25, h * .5 - 5, 2, self.Header.VBarHeight)
+            surface.DrawRect(w - 25, h * .5 + 2, 2, self.Header.VBarHeight)
+        end
+    end
+end
+
+function PANEL:OnToggle(expanded)
+    local to = 0
+
+    if self.Header.VBarHeight < 5 then
+        to = 5
+    end
+
+    local anim = self.Header:NewAnimation(.1, 0, 1)
+    anim.Think = function()
+        self.Header.VBarHeight = math.Approach(self.Header.VBarHeight,to,1)
+    end
+
+end
+
+function PANEL:SetFont(font)
+    self.Header:SetFont(font)
+end
+
+function PANEL:SetColor(color)
+    self.Color = color
 end
 
 derma.DefineControl("DynCollapsibleCategory", "Dynamic VGUI collapsible category", PANEL, "DCollapsibleCategory")
